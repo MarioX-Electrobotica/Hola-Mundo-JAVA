d465cd876e0d3db8d521770949470647b0fba5c9
Electrobotica <157917059+MarioX-Electrobotica@users.noreply.github.com>
2024-04-24 23:17:38 -0400

    Hola Mundo, JAVA
    Programacion orientado a objetos con JAVA

---
+++ HolaMundo.java
@@ -0,0 +1,17 @@
+
+/**
+ * Programa para imprimer texto
+ * empresa ELECTROBOTICA
+ * @author Mario Quispe Gutierrez 
+ * @version (20240523)
+ */
+//es es un clase y el metodo clase es HolaMundo
+public class HolaMundo
+{
+    // el motodo  main empieza la ejucucion de la aplicacion en java
+    public static void main(String[]args){
+       // Con este codigo imprime el texto que lo puedo observar en su terminal
+        System.out.println("Hola Mundo y Bienvenido ala programacion en JAVA");
+    }//fin del metodo main
+    
+}//fin del clase Hola Mundo
